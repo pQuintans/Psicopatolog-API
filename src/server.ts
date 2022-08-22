dc2c09e46a7598470709dc16aa52e6a5206cec0a
pQuintans <caixote.dom@gmail.com>
2022-08-21 21:28:02 -0300

    Adicionando arquivos

--- src/server.ts
+++ src/server.ts
@@ -19,6 +19,7 @@ const app = express()
 app.use(express.json())
 app.use(cors(options))
 app.use(cookieParser())
+app.use('/files', express.static('files'))
 app.use(router)
 
 app.use(handleErrorsMiddleware)
